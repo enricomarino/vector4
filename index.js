276f3e7d89879ac50883a8a9a1c866a96f90d889
Enrico Marino <enrico.marino@email.com>
2012-09-16 18:42:03 +0200

    added vector4.create

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-module.exports = require('./lib/vector4.js');
+/* !
+ * vector4
+ * JavaScript math library for 4d vector
+ * Copyright (c) 2012 Enrico Marino (http://onirame.no.de)
+ * MIT License
+ */
+
+ !(function (exports) {
+
+  /**
+   * Library namespace.
+   */
+
+  var vector4 = exports.vector4 = {};
+
+  /**
+   * Library version.
+   */
+
+  vector4.version = '0.0.0';
+
+  /**
+   * create
+   * Create a 4d vector.
+   * 
+   * @return {Float32Array} 3d vector
+   * @api public
+   */
+
+  vector4.create = function () {
+    return new Float32Array([0.0, 0.0, 0.0, 0.0]);
+  };
+
+}(this));
